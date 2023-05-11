6a2c63ba1b6c4cae6020f7006402191e20690b5a
ffauchille <florent.fauchille@bettermarks.com>
2023-05-11 12:48:05 +0200

    dockerfiles

--- run-unit-test.sh
+++ run-unit-test.sh
@@ -4,4 +4,4 @@ if [ -z $PORT ]; then
     echo "MISSING PORT ARG"
     exit 1
 fi
-curl -XPOST -H "Content-Type: application/json" -d '{"position": {"lat": 48.8115336, "lng": 2.3681119}}' "http://localhost:$PORT/v1/products?radius=15"
+curl -XPOST -H "Content-Type: application/json" -d '{"position": {"lat": 48.8115336, "lng": 2.3681119}}' "http://127.0.0.1:$PORT/v1/products?radius=15"
